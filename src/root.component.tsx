aee809f60b3d8453f32e05e6303089ffefa60b17
Jacob Holmes <jacob.holmes@coxautoinc.com>
2021-03-24 09:29:41 -0700

    initial commit

---
+++ src/root.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Root(props) {
+  return (
+    <section className="flex flex-row justify-center p-32">
+      <div className="text-black text-4xl">Welcome to my single spa demo</div>
+    </section>
+  );
+}
