c6dadb94f512c73acd8c1ea0569573d37b726083
Jacob Holmes <jacob.holmes@coxautoinc.com>
2021-03-25 17:54:07 -0700

    build fixes

--- build/after_deploy.sh
+++ build/after_deploy.sh
@@ -3,7 +3,7 @@ NEW_URL="https://single-spa-react-demo.s3.amazonaws.com/dashboard/${GITHUB_SHA}/
 echo "Downloading import map from S3"
 aws s3 cp s3://single-spa-react-demo/importmap.json importmap.json
 echo "Updating import map to point to new version of"
-cat importmap.json | jq -r '.imports["@cd/dashboard"] = "${NEW_URL}"' importmap.json
+echo $(jq --arg variable "$NEW_URL" '.imports["@cd/models"] = $variable' importmap.json) > importmap.json
 echo "Uploading new import map to S3"
 aws s3 cp importmap.json s3://single-spa-react-demo/importmap.json --cache-control 'public, must-revalidate, max-age=0' --acl 'public-read'
 echo "Deployment successful"
