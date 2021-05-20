460bcd174e12dc3b888ef2b2c57944d643ac453c
Jacob Holmes <holmes315@gmail.com>
2021-05-20 14:41:39 -0700

    Update root.component.tsx

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function Root(props) {
-  return (
-    <section className="flex flex-row justify-center p-32">
-      <div className="text-black text-4xl">Welcome to my single spa demo</div>
-    </section>
-  );
+  return <section>{props.name} is mounted!</section>;
 }
